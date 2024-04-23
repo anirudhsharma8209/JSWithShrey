bf96a9af0409e52f238b169216f1ea341918c5c7
anirudhsharma8209 <devprakashsharma7790@gmail.com>
2024-04-23 10:56:38 +0530

    array and object des....

---
+++ Module.js
@@ -0,0 +1,29 @@
+// Array or Object
+
+// let arr = ["Anirudh", 21, ["14/239", "xyz", 305001], "xyz", "abc"];
+// let Obj = {
+//     name : "Anirudh",
+//     fullName : {first: "Anirudh", last : "Sharma"},
+//     fullage : 21
+// };
+
+
+// Global scope or global execution context xyz = 100;
+
+// let {fullage, fullName} = Obj
+// let {name, fullName} = Obj;
+// console.log(fullName.first)
+// console.log(fullName)
+// let [name, age] = arr;
+// console.log(name)
+// console.log(age)
+
+// {name, age} = Obj;
+// console.log(name)
+// [user1, user2, user3, user4, user5] = arr;
+// console.log(name)
+// console.log(age)
+// [add1, add2] = address
+// console.log(array)
+// [name1, name2] = array;
+// console.log(arr)
